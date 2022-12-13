ddc1c25445831eafc68f3fa5a0f24902129b5e2d
Bouman <ndjbouman@gmail.com>
2022-12-13 15:07:09 +0100

    Formulaire register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./assets/css/App.css";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
 import HomePage from "./pages/Home";
 import ProfilePage from "./pages/Profile";
 import SettingsPage from "./pages/Settings";
@@ -10,17 +11,16 @@ import HomeLayout from "./components/HomeLayout";
 function App() {
   return (
     <Routes>
-
       <Route element={<HomeLayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
 
       <Route path="/dashboard" element={<ProtectedLayout />}>
         <Route path="profile" element={<ProfilePage />} />
         <Route path="settings" element={<SettingsPage />} />
       </Route>
-
     </Routes>
   );
 }
