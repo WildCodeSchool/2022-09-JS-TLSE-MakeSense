d540cbd73f15522b145dd8ddad52fd178947d5e8
Bouman <ndjbouman@gmail.com>
2022-12-20 11:45:52 +0100

    Update Najim branch

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-const itemControllers = require("./controllers/itemControllers");
+const usersControllers = require("./controllers/usersControllers");
 const langControllers = require("./controllers/langControllers");
 
 const scriptfs = require("./scripts/fs");
@@ -10,10 +10,10 @@ router.post("/readfs", scriptfs.readallfiles);
 
 router.get("/lang", langControllers.langlist);
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+router.get("/users", usersControllers.browse);
+router.get("/users/:id", usersControllers.read);
+router.put("/users/:id", usersControllers.edit);
+router.post("/users", usersControllers.add);
+router.delete("/users/:id", usersControllers.destroy);
 
 module.exports = router;
