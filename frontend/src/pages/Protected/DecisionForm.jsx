30690f2ff16e47318b3910216583e88e5699f867
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-01-04 09:56:41 +0100

    add field composant

--- frontend/src/pages/Protected/DecisionForm.jsx
+++ frontend/src/pages/Protected/DecisionForm.jsx
@@ -1,8 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "../../assets/css/header/AppBar.css";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useForm } from "react-hook-form";
 
-export function DatePick() {
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link"],
+    ["clean"],
+  ],
+};
+
+function DatePick() {
   const [date, setDate] = useState(new Date());
   const onChange = (thedate) => {
     setDate(thedate);
@@ -14,45 +33,46 @@ export function DatePick() {
   );
 }
 
-function DecisionForm() {
+function Field(props) {
   return (
     <div>
-      <h1>Déposer une décision</h1>;
-      <form>
+      <label htmlFor="email">{props.name}</label>
+      <ReactQuill theme="snow" value={props.value} modules={modules} />
+    </div>
+  );
+}
+
+function DecisionForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  // // =>>>>>> https://www.react-hook-form.com/
+  // // https://codesandbox.io/s/cdjru?file=/src/App.js:325-427l
+
+  // useEffect(() => {
+  //   register("", { required: true, minLength: 11 });
+  // }, [register]);
+
+  return (
+    <div>
+      <h1>Déposer une décision</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <fieldset>
           <legend>Décrire tous les éléments de sa décision</legend>
           <div>
-            <label htmlFor="title">Title</label>
+            <label htmlFor="title">Titre</label>
             <br />
             <input type="text" name="title" id="title" required />
           </div>
-          <div>
-            <label htmlFor="email">Description de la décision</label>
-          </div>
-          <div style={{ color: "red", width: "100px" }}>
-            <label htmlFor="email">Utilité de la décision</label>
-            {
-              // WYSIWYG
-            }
-          </div>
-          <div>
-            <label htmlFor="email">Contexte</label>
-            {
-              // WYSIWYG
-            }
-          </div>
-          <div>
-            <label htmlFor="email">Bénéfices</label>
-            {
-              // WYSIWYG
-            }
-          </div>
-          <div>
-            <label htmlFor="email">Inconvénients</label>
-            {
-              // WYSIWYG
-            }
-          </div>
+          <Field name="Description de la décision" value="description" />
+          <Field name="Utilité pour l'organisation" value="utility" />
+          <Field name="Contexte autour de la décision" value="context" />
+          <Field name="Bénéfices" value="pros" />
+          <Field name="Inconvénients" value="cons" />
         </fieldset>
         {
           // button pass to next
@@ -60,8 +80,8 @@ function DecisionForm() {
         <button type="button">Passer aux concernés</button>
         <fieldset>
           <legend>Définir les concernés et les experts</legend>
-          <button type="button">Définir le calendrier</button>
         </fieldset>
+        <button type="button">Définir le calendrier</button>
         <fieldset>
           <legend>Définir le calendrier</legend>
           <select name="pets" id="pet-select">
@@ -78,7 +98,6 @@ function DecisionForm() {
           <DatePick />
           <input type="checkbox" />
           <p>Ne pas définir de date pour cette étape</p>
-
           <div>
             <input type="submit" value="Poster ma décision ! Youpiiiii" />
           </div>
