ab4bca9ad9d5f886ae6e0b5e072e5500dc9814a1
Najim KEBDANI <najimkeb31@gmail.com>
2023-01-09 11:48:01 +0100

    searchbar filtre

--- frontend/src/components/container/Protected/SearchBar.jsx
+++ frontend/src/components/container/Protected/SearchBar.jsx
@@ -5,37 +5,26 @@ import Card from "./Card";
 // eslint-disable-next-line react/prop-types
 function SearchBar({ datas }) {
   const [searchTerm, setSearchTerm] = useState();
-  const [results, setResults] = useState([]);
   function handleChange(event) {
     setSearchTerm(event.target.value);
   }
-  function handleSubmit(event) {
-    event.preventDefault();
-    // Appel à une fonction pour effectuer la recherche avec `searchTerm`
-    // setResults(searchTerm).then((results) => {
-    // setResults(results);
-  }
-  // const filteredData = this.state.data.filter((data) =>
-  // data.content.toLowerCase().includes(this.state.handleChange.toLowerCase())
-  // );
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <input type="text" value={searchTerm} onChange={handleChange} />
-      <button type="submit">🔎</button>
       <div>
         {
           // eslint-disable-next-line react/prop-types
-          datas.map((data) => (
-            <Card data={data.id} content={data.content} />
-          ))
+          datas
+            // eslint-disable-next-line react/prop-types
+            .filter((data) =>
+              JSON.parse(data.content).title.includes(searchTerm)
+            )
+            .map((data) => (
+              <Card key={data.id} data={data} />
+            ))
         }
       </div>
-      {
-        // filteredData.map(() => {
-        // <Card data={data.id} content={data.content} />;
-        // })
-      }
     </form>
   );
 }
