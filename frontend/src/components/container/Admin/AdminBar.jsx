8c04da611ab3537414186912435d45887b698132
STEPHANIE MALBERT <[STEPHANIE.MALBERT@live.fr](mailto:STEPHANIE.MALBERT@live.fr)>
2023-01-10 12:09:03 +0100

    Ok

--- frontend/src/components/container/Admin/AdminBar.jsx
+++ frontend/src/components/container/Admin/AdminBar.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
-import AdminBarCss from "../../../assets/css/container/Admin/AdminBar.css";
 import { Text } from "../../../contexts/Language";
-import "@assets/css/container/admin/AdminBar.css";
+import "@assets/css/container/Admin/adminBar.css";
 
 function AdminBar({ menuadmin }) {
   const [anchorElNav, setAnchorElNav] = useState(null);
