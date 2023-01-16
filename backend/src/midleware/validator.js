f874b563067aa15c3622510c2f48f79729a569c2
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-01-16 14:12:11 +0100

    change everything to simplify form

--- backend/src/midleware/validator.js
+++ backend/src/midleware/validator.js
@@ -9,10 +9,29 @@ const userSchema = Joi.object({
   admin: Joi.number().integer().max(1).required(),
 });
 
+const decisionSchema = Joi.object({
+  title: Joi.string().min(5).max(250).message("Title is required").required(),
+  description: Joi.string()
+    .min(1)
+    .message("Description is required")
+    .required(),
+  context: Joi.string().min(1).required(),
+  utility: Joi.string().min(1).required(),
+  pros: Joi.string().min(1).required(),
+  cons: Joi.string().min(1).required(),
+  impacted: Joi.array().min(0),
+  experts: Joi.array().min(0),
+  firstDate: Joi.date().required(),
+  dateOpinion: Joi.date().required(),
+  dateFirstDecision: Joi.date().required(),
+  dateEndConflict: Joi.date().required(),
+  dateFinaleDecision: Joi.date().required(),
+});
+
 const validateUser = (req, res, next) => {
   const { lastname, firstname, email, password, serviceId, admin } = req.body;
 
-  const { error } = userSchema.validate(
+  const { error } = decisionSchema.validate(
     { lastname, firstname, email, password, serviceId, admin },
     { abortEarly: false }
   );
@@ -24,6 +43,19 @@ const validateUser = (req, res, next) => {
   }
 };
 
+const validateDecision = (req, res, next) => {
+  const { decision } = req.body;
+
+  const { error } = userSchema.validate({ decision }, { abortEarly: false });
+
+  if (error) {
+    res.status(422).json({ validationErrors: error.details });
+  } else {
+    next();
+  }
+};
+
 module.exports = {
   validateUser,
+  validateDecision,
 };
--- frontend/src/pages/Protected/DecisionForm.jsx
+++ frontend/src/pages/Protected/DecisionForm.jsx
@@ -1,19 +1,14 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useMemo, useEffect } from "react";
 import Concerned from "@components/form/Concerned";
 // imports WYSIWYG
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-// import to get datas on submit
-import { useForm, Controller } from "react-hook-form";
-import { joiResolver } from "@hookform/resolvers/joi";
-import Joi from "joi";
 // imports CSS
 import "../../assets/css/header/AppBar.css";
 import "../../assets/css/form/form.css";
 // imports DatePicker
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { format } from "date-fns";
 import api from "@services/api";
 
 function DecisionForm() {
@@ -33,61 +28,22 @@ function DecisionForm() {
     ],
   };
 
-  // const schema = Joi.object({
-  //   title: Joi.string().min(5).max(250).message("Title is required").required(),
-  //   description: Joi.string()
-  //     .min(1)
-  //     .message("Description is required")
-  //     .required(),
-  //   context: Joi.string().min(1).message("Context is required").required(),
-  //   utility: Joi.string().min(1).message("Utility is required").required(),
-  //   pros: Joi.string().min(1).message("Pros are required").required(),
-  //   cons: Joi.string().min(1).message("Cons are required").required(),
-  //   impacted: Joi.array().min(0).message("not working"),
-  //   experts: Joi.array().min(0).message("not working"),
-  //   firstDate: Joi.string().min(10).message("Date is required").required(),
-  //   dateOpinion: Joi.string().min(10).message("Date is required").required(),
-  //   dateFirstDecision: Joi.string()
-  //     .min(10)
-  //     .message("Date is required")
-  //     .required(),
-  //   dateEndConflict: Joi.string()
-  //     .min(10)
-  //     .message("Date is required")
-  //     .required(),
-  //   dateFinaleDecision: Joi.string()
-  //     .min(10)
-  //     .message("Date is required")
-  //     .required(),
-  // });
-
   const [users, setUsers] = useState();
   const [isLoaded, setIsLoaded] = useState(false);
-  const [impacted, setImpacted] = useState([]);
-  const [experts, setExpert] = useState([]);
-  const [firstDate, setFirstDate] = useState(new Date());
-  const [dateOpinion, setDateOpinion] = useState(new Date());
-  const [dateFirstDecision, setDateFirstDecision] = useState(new Date());
-  const [dateEndConflict, setDateEndConflict] = useState(new Date());
-  const [dateFinaleDecision, setDateFinaleDecision] = useState(new Date());
-
-  const { handleSubmit, setValue, getFieldState } = useForm({
-    defaultValues: {
-      title: "",
-      description: "",
-      context: "",
-      utility: "",
-      pros: "",
-      cons: "",
-      impacted: [],
-      experts: [],
-      firstDate: "",
-      dateOpinion: "",
-      dateFirstDecision: "",
-      dateEndConflict: "",
-      dateFinaleDecision: "",
-    },
-    // resolver: joiResolver(schema),
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+    utility: "",
+    context: "",
+    pros: "",
+    cons: "",
+    impacted: [],
+    experts: [],
+    firstDate: new Date(),
+    dateOpinion: new Date(),
+    dateFirstDecision: new Date(),
+    dateEndConflict: new Date(),
+    dateFinaleDecision: new Date(),
   });
 
   const getUsers = async () => {
@@ -102,33 +58,25 @@ function DecisionForm() {
     getUsers();
   }, [isLoaded]);
 
-  // useEffect(() => {
-  //   setValue("impacted", impacted);
-  //   setValue("experts", experts);
-  // }, [impacted, experts]);
-
-  function onSubmit(data) {
-    setValue("impacted", impacted);
-    setValue("experts", experts);
-    console.warn(data);
-
-    // const body = {
-    //   content: JSON.stringify(data),
-    //   status: 1,
-    //   id_user_creator: 9,
-    // };
-    // return api
-    //   .apipostmysql(`${import.meta.env.VITE_BACKEND_URL}/decisions`, body)
-    //   .then((json) => {
-    //     return json;
-    //   });
+  function handleSubmit(e) {
+    e.preventDefault();
+    const body = {
+      content: JSON.stringify(form),
+      status: 1,
+      id_user_creator: 9,
+    };
+    return api
+      .apipostmysql(`${import.meta.env.VITE_BACKEND_URL}/decisions`, body)
+      .then((json) => {
+        return json;
+      });
   }
 
   return (
     isLoaded && (
       <div>
         <h1>Déposer une décision</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit}>
           <legend className="hello">
             Décrire tous les éléments de sa décision
           </legend>
@@ -138,151 +86,131 @@ function DecisionForm() {
             type="text"
             name="title"
             id="title"
-            // eslint-disable-next-line react/jsx-props-no-spreading
-            onChange={(editorState) => {
-              setValue("title", editorState);
+            value={form.title}
+            onChange={(event) => {
+              setForm({ ...form, [event.target.name]: event.target.value });
             }}
           />
-          {/* <p>{errors.title?.message}</p> */}
           <label htmlFor="description">Description de la décision</label>
           <ReactQuill
             theme="snow"
             modules={modules}
-            value=""
-            onChange={(editorState) => {
-              console.warn(editorState);
-              setValue("description", editorState);
-              const test = getFieldState("description");
-              console.warn(test);
+            name="description"
+            value={form.description}
+            onChange={(event) => {
+              setForm({ ...form, description: event });
             }}
           />
-          {/* <p>{errors.description?.message}</p> */}
           <label htmlFor="utility">Utilité pour l'organisation</label>
           <ReactQuill
             theme="snow"
             modules={modules}
-            value=""
-            onChange={(editorState) => {
-              setValue("utility", editorState);
+            name="utility"
+            value={form.utility}
+            onChange={(event) => {
+              setForm({ ...form, utility: event });
             }}
           />
-          {/* <p>{errors.utility?.message}</p> */}
           <label htmlFor="context">Contexte autour de la décision</label>
           <ReactQuill
             theme="snow"
             modules={modules}
-            value=""
-            onChange={(editorState) => {
-              setValue("context", editorState);
+            name="context"
+            value={form.context}
+            onChange={(event) => {
+              setForm({ ...form, context: event });
             }}
           />
-          {/* <p>{errors.context?.message}</p> */}
           <label htmlFor="pros">Bénéfices</label>
           <ReactQuill
             theme="snow"
             modules={modules}
-            value=""
-            onChange={(editorState) => {
-              setValue("pros", editorState);
+            value={form.pros}
+            onChange={(event) => {
+              setForm({ ...form, pros: event });
             }}
           />
-          {/* <p>{errors.pros?.message}</p> */}
           <label htmlFor="cons">Inconvénients</label>
           <ReactQuill
             theme="snow"
             modules={modules}
-            value=""
-            onChange={(editorState, e) => {
-              setValue("cons", editorState);
+            value={form.cons}
+            onChange={(event) => {
+              setForm({ ...form, cons: event });
             }}
           />
-          {/* <p>{errors.cons?.message}</p> */}
 
           <legend>Définir les concernés et les experts</legend>
           <Concerned
             table={users}
             name="concernés"
-            type={impacted}
-            updateType={setImpacted}
+            type={form.impacted}
+            updateType={(event) => setForm({ ...form, impacted: event })}
           />
-          {/* <p>{errors.impacted?.message}</p> */}
 
           <Concerned
             table={users}
             name="experts"
-            type={experts}
-            updateType={setExpert}
+            type={form.experts}
+            updateType={(event) => setForm({ ...form, experts: event })}
           />
-          {/* <p>{errors.experts?.message}</p> */}
           <fieldset>
             <legend>Définir le calendrier</legend>
             <div className="datepicker">
               <p>Date de dépôt de la décision</p>
               <DatePicker
-                selected={firstDate}
-                minDate={firstDate}
-                maxDate={firstDate}
+                selected={form.firstDate}
+                minDate={form.firstDate}
+                maxDate={form.firstDate}
                 onChange={(d) => {
-                  setFirstDate(d);
-                  setValue("firstDate", format(d, "dd-MM-yyyy"));
+                  setForm({ ...form, firstDate: d });
                 }}
               />
-              {/* <p>{errors.firstDate?.message}</p> */}
             </div>
             <div className="datepicker">
               <p>Fin de la prise des avis</p>
               <DatePicker
-                selected={dateOpinion}
-                minDate={firstDate}
+                selected={form.dateOpinion}
+                minDate={form.firstDate}
                 onChange={(d) => {
-                  setDateOpinion(d);
-                  setValue("dateOpinion", format(d, "dd-MM-yyyy"));
+                  setForm({ ...form, dateOpinion: d });
                 }}
               />
-              {/* <p>{errors.dateOpinion?.message}</p> */}
             </div>
             <div className="datepicker">
               <p>Fin de la première décision</p>
               <DatePicker
-                selected={dateFirstDecision}
-                minDate={dateOpinion}
+                selected={form.dateFirstDecision}
+                minDate={form.dateOpinion}
                 onChange={(d) => {
-                  setDateFirstDecision(d);
-                  setValue("dateFirstDecision", format(d, "dd-MM-yyyy"));
+                  setForm({ ...form, dateFirstDecision: d });
                 }}
               />
-              {/* <p>{dateFirstDecision.firstDate?.message}</p> */}
             </div>
             <div className="datepicker">
               <p>Fin du conflit sur la première décision</p>
               <DatePicker
-                selected={dateEndConflict}
-                minDate={dateFirstDecision}
+                selected={form.dateEndConflict}
+                minDate={form.dateFirstDecision}
                 onChange={(d) => {
-                  setDateEndConflict(d);
-                  setValue("dateEndConflict", format(d, "dd-MM-yyyy"));
+                  setForm({ ...form, dateEndConflict: d });
                 }}
               />
-              {/* <p>{errors.dateEndConflict?.message}</p> */}
             </div>
             <div className="datepicker">
               <p>Décision définitive</p>
               <DatePicker
-                selected={dateFinaleDecision}
-                minDate={dateEndConflict}
+                selected={form.dateFinaleDecision}
+                minDate={form.dateEndConflict}
                 onChange={(d) => {
-                  setDateFinaleDecision(d);
-                  setValue("dateFinaleDecision", format(d, "dd-MM-yyyy"));
+                  setForm({ ...form, dateFinaleDecision: d });
                 }}
               />
-              {/* <p>{errors.dateFinaleDecision?.message}</p> */}
             </div>
           </fieldset>
-          <input
-            type="submit"
-            className="buttonForm"
-            value="Poster ma décision ! Youpiiiii"
-          />
+          <button type="submit" className="buttonForm">
+            Poster ma décision
+          </button>
         </form>
       </div>
     )
