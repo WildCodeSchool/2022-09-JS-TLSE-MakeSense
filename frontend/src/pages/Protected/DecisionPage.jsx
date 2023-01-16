500f9de6987354d9f699f20fd78724838114d432
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-01-16 16:22:29 +0100

    fix asynchronicity about decision/user

--- frontend/src/pages/Protected/DecisionPage.jsx
+++ frontend/src/pages/Protected/DecisionPage.jsx
@@ -9,162 +9,131 @@ function Decisions() {
   const [impacted, setImpacted] = useState();
   const [expert, setExpert] = useState();
   const [isLoaded, setIsLoaded] = useState(false);
-  const id = 24;
-
-  const getDecisions = async () => {
-    // get the decision
-    const callDecisions = await api.apigetmysql(
-      `${import.meta.env.VITE_BACKEND_URL}/decisionpage/${id}`
-    );
-    setDecisions(callDecisions);
-  };
-
-  // get users (writer, impacted, experts) by id
-  const getUsers = async () => {
-    const callUserById = await api.apigetmysql(
-      `${import.meta.env.VITE_BACKEND_URL}/users/${parseInt(
-        decisions.id_user_creator,
-        10
-      )}`
-    );
-    // const callImpactedById = await api.apigetmysql(
-    //   `${import.meta.env.VITE_BACKEND_URL}/users/${parseInt(
-    //     JSON.parse(decisions.content).impacted[0].id,
-    //     10
-    //   )}`
-    // );
-    // const callExpertById = await api.apigetmysql(
-    //   `${import.meta.env.VITE_BACKEND_URL}/users/${parseInt(
-    //     JSON.parse(decisions.content).experts[0].id,
-    //     10
-    //   )}`
-    // );
-
-    setUser(callUserById);
-    setImpacted(JSON.parse(decisions.content).impacted);
-    setExpert(JSON.parse(decisions.content).experts);
-    setIsLoaded(true);
-  };
+  const id = 29;
 
   useEffect(() => {
-    getDecisions();
-  }, [isLoaded]);
-  useEffect(() => {
-    getUsers();
-  }, [decisions]);
+    const getAllApis = async () => {
+      // get the decision
+      const callDecisions = await api.apigetmysql(
+        `${import.meta.env.VITE_BACKEND_URL}/decisionpage/${id}`
+      );
+      setDecisions(callDecisions);
+      const callUserById = await api.apigetmysql(
+        `${import.meta.env.VITE_BACKEND_URL}/users/${
+          callDecisions.id_user_creator
+        }`
+      );
+      setUser(callUserById);
+      setImpacted(JSON.parse(callDecisions.content).impacted);
+      setExpert(JSON.parse(callDecisions.content).experts);
+      setIsLoaded(true);
+    };
+    getAllApis();
+  }, []);
 
-  return (
-    isLoaded && (
-      <div className="decisionContainer">
-        <div className="decisionDetailsContainer">
-          <div>
-            <h1>{JSON.parse(decisions.content).title}</h1>
-            <div>
-              Par {user.firstname} {user.lastname}
-            </div>
-            <div>
-              Date de création : {decisions.date_created.substring(0, 10)}
-            </div>
-            <div>Mis à jour : {decisions.date_update.substring(0, 10)}</div>
-          </div>
-          {
-            // ou utiliser html-react-parser
-          }
-          <div>
-            <details>
-              <summary>Description</summary>
-              <div
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: JSON.parse(decisions.content).description,
-                }}
-              />
-            </details>
-            <details>
-              <summary>Contexte</summary>
-              <div
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: JSON.parse(decisions.content).context,
-                }}
-              />
-            </details>
-            <details>
-              <summary>Utilité</summary>
-              <div
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: JSON.parse(decisions.content).utility,
-                }}
-              />
-            </details>
-            <details>
-              <summary>Avantages</summary>
-              <div
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: JSON.parse(decisions.content).pros,
-                }}
-              />
-            </details>
-            <details>
-              <summary>Inconvénients</summary>
-              <div
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: JSON.parse(decisions.content).cons,
-                }}
-              />
-            </details>
-            <CommentSection id={id} />
-          </div>
-        </div>
+  return isLoaded ? (
+    <div className="decisionContainer">
+      <div className="decisionDetailsContainer">
         <div>
-          <h3>Première décision prise</h3>
-          <div
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: JSON.parse(decisions.content).firstDecision.substring(
-                0,
-                10
-              ),
-            }}
-          />
-          <h3>Fin de la période de conflit</h3>
-          <div
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: JSON.parse(decisions.content).endConflict.substring(
-                0,
-                10
-              ),
-            }}
-          />
-          <h3>Décision finale</h3>
-          <div
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: JSON.parse(decisions.content).finaleDecision.substring(
-                0,
-                10
-              ),
-            }}
-          />
-          <h3>Personnes impactées</h3>
+          <h1>{JSON.parse(decisions.content).title}</h1>
           <div>
-            {impacted.map((person) => (
-              <div>{person.text}</div>
-            ))}
+            Par {user.firstname} {user.lastname}
           </div>
-          <h3>Personnes expertes</h3>
           <div>
-            {expert.map((person) => (
-              <div>{person.text}</div>
-            ))}
+            Date de création : {decisions.date_created.substring(0, 10)}
           </div>
+          <div>Mis à jour : {decisions.date_update.substring(0, 10)}</div>
+        </div>
+        {
+          // ou utiliser html-react-parser
+        }
+        <div>
+          <details>
+            <summary>Description</summary>
+            <div
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: JSON.parse(decisions.content).description,
+              }}
+            />
+          </details>
+          <details>
+            <summary>Contexte</summary>
+            <div
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: JSON.parse(decisions.content).context,
+              }}
+            />
+          </details>
+          <details>
+            <summary>Utilité</summary>
+            <div
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: JSON.parse(decisions.content).utility,
+              }}
+            />
+          </details>
+          <details>
+            <summary>Avantages</summary>
+            <div
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: JSON.parse(decisions.content).pros,
+              }}
+            />
+          </details>
+          <details>
+            <summary>Inconvénients</summary>
+            <div
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: JSON.parse(decisions.content).cons,
+              }}
+            />
+          </details>
+          <CommentSection id={id} />
         </div>
       </div>
-    )
+      <div>
+        <h3>Première décision prise</h3>
+        <div
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: JSON.parse(decisions.content).firstDecision,
+          }}
+        />
+        <h3>Fin de la période de conflit</h3>
+        <div
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: JSON.parse(decisions.content).endConflict,
+          }}
+        />
+        <h3>Décision finale</h3>
+        <div
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: JSON.parse(decisions.content).finaleDecision,
+          }}
+        />
+        <h3>Personnes impactées</h3>
+        <div>
+          {impacted.map((person) => (
+            <div key={person.id}>{person.text}</div>
+          ))}
+        </div>
+        <h3>Personnes expertes</h3>
+        <div>
+          {expert.map((person) => (
+            <div key={person.id}>{person.text}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div>Is loading</div>
   );
 }
 export default Decisions;
