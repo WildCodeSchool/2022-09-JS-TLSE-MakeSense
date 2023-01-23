b1a405a912a57318fadaf84d18ba66ff8046bcf8
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-01-23 10:14:21 +0100

    fix some styles

--- frontend/src/components/container/Admin/Decisions.jsx
+++
@@ -1,8 +0,0 @@
-function Decisions() {
-  return (
-    <div className="user-admin-wrapper">
-      <h1>Decisions components</h1>
-    </div>
-  );
-}
-export default Decisions;
--- frontend/src/components/header/AppBar.jsx
+++ frontend/src/components/header/AppBar.jsx
@@ -68,7 +68,7 @@ function AppBar({ menu }) {
           <div className="flex">
             <button
               type="button"
-              className="text-white bg-calypso hover:bg-calypsoLight focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              className="text-white bg-calypso hover:bg-calypsoLight font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
               onClick={() => {
                 navigate(`/user/decisions?comp=Form`);
               }}
