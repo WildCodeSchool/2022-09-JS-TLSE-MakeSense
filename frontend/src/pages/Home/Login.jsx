34a506848dce70272208c035c498fd2422be703a
Najim KEBDANI <najimkeb31@gmail.com>
2023-01-30 09:51:12 +0100

    traduction Connexion

--- frontend/src/pages/Home/Login.jsx
+++ frontend/src/pages/Home/Login.jsx
@@ -73,14 +73,16 @@ export default function LoginPage() {
   ) : (
     <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8">
       <div className="login">
-        <h1 className="text-xl font-medium text-gray-900">Log In</h1>
+        <h1 className="text-xl font-medium text-gray-900">
+          <Text tid="login" />
+        </h1>
         <form className="space-y-6" onSubmit={handleSubmit}>
           <div>
             <label
               htmlFor="email"
               className="block mb-2 text-sm font-medium text-gray-900"
             >
-              Your email
+              <Text tid="email" />
             </label>
             <input
               pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
@@ -93,18 +95,21 @@ export default function LoginPage() {
               autoComplete="email"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             />
-            <span className="form__error">email erroné</span>
+            <span className="form__error">
+              {" "}
+              <Text tid="wrongemail" />
+            </span>
           </div>
           <div>
             <label
               htmlFor="password"
               className="block mb-2 text-sm font-medium text-gray-900"
             >
-              Your password
+              <Text tid="password" />
             </label>
             <input
               pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
-              placeholder="Password"
+              placeholder="Mot de passe"
               required
               id="password"
               name="password"
@@ -114,21 +119,22 @@ export default function LoginPage() {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             />
             <span className="text-rose-500 text-xs">
-              Minimum 8 caracteres, une majuscule, une minuscule, un chiffre, un
-              caractère spécial.
+              <Text tid="minimum8charactersoneuppercaseonelowercase" />
             </span>
           </div>
           <button
             type="submit"
             className="w-full text-white bg-calypso hover:bg-calypsoLight font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           >
-            Login to your account
+            <Text tid="logintoyouraccount" />
           </button>
           <div className="text-sm font-medium text-gray-500">
-            Not registered?{" "}
+            <Text tid="notregistered?" />{" "}
             <span href="#" className="text-blue-700 hover:underline">
               {" "}
-              <Link to="/register">Don't have an account yet? Sign Up</Link>
+              <Link to="/register">
+                <Text tid="Vous n'avez pas encore de compte? S'inscrire" />
+              </Link>
             </span>
           </div>
         </form>
